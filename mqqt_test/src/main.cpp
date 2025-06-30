2429417b948fb3a6070b8ece35a72f62fd09e8a0
Kchun510 <kylechun1@gmail.com>
2025-06-30 16:47:28 -0700

    Button input set up, going to try and send a packet now

--- mqqt_test/src/main.cpp
+++ mqqt_test/src/main.cpp
@@ -1,16 +1,20 @@
 #include "../main.h"
 #include <Arduino.h>
 
-const int ledPin_external = 24;
+const int ledPin_external = 22;
+const int button_input = 23;
 
 void init_system(){
 	pinMode(ledPin_external, OUTPUT);
+	pinMode(button_input, INPUT);
 }
 
 void blink_led(){
-	digitalWrite(ledPin_external, HIGH);
-	delay(3000);
-	digitalWrite(ledPin_external, LOW);
-	delay(3000);
+	if(digitalRead(button_input) == HIGH){
+		digitalWrite(ledPin_external, HIGH);
+		delay(3000);
+		digitalWrite(ledPin_external, LOW);
+		delay(3000);
+	}
 }
 
