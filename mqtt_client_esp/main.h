106ac72f9f6a4a0ac000251c62e9b71da69bfa23
Kchun510 <kylechun1@gmail.com>
2025-06-30 17:36:31 -0700

    Naming update

--- mqtt_client_esp/main.h
+++ mqtt_client_esp/main.h
@@ -2,3 +2,6 @@
 
 void init_system();
 void blink_led();
+void setup_wifi();
+void reconnect();
+void setup();
---
+++ server_mqtt/server.ts
@@ -0,0 +1,19 @@
+import Aedes, { Client, PublishPacket } from 'aedes';
+import net from 'net';
+
+const broker = new Aedes();
+
+const server = net.createServer(broker.handle);
+
+server.listen(1883, () => {
+	console.log('Aedes MQTT broker running on port 1883');
+});
+
+broker.on('client', (client: Client) => {
+	console.log('Client connected:', client?.id);
+});
+
+broker.on('publish', (packet: PublishPacket, client: Client | null) => {
+	console.log(`Message on ${packet.topic}: ${packet.payload.toString()}`);
+});
+
