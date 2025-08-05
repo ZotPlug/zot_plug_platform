e47af3f95b2b8486899011928413c54e82293ace
Kchun510 <kylechun1@gmail.com>
2025-08-05 13:17:55 -0700

    Additions, to make ssl cert renewal automatic

---
+++ infra/certbot/renew_script.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+trap exit TERM
+
+while :; do
+	sleep 12h & wait $!
+	certbot renew --webroot -w /data/letsencrypt \
+		--deploy-hook 'touch /data/letsencrypt/RELOAD_NGINX'
+done
+
+
+
+
