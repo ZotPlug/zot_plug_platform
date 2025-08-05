1dff069fae6a7412916f9b8e6b5ed44dd7798472
Kchun510 <kylechun1@gmail.com>
2025-08-05 14:08:17 -0700

    The certbot image, does not use bash. It uses sh

--- infra/certbot/renew_script.sh
+++ infra/certbot/renew_script.sh
@@ -1,9 +1,10 @@
-#!/bin/bash
+#!/bin/sh
 
 trap exit TERM
 
+echo "[auto-cert-renewal] Starting auto-cert-renewal"
+
 while :; do
 	sleep 12h & wait $!
-	certbot renew --webroot -w /data/letsencrypt \
-		--deploy-hook 'touch /data/letsencrypt/RELOAD_NGINX'
+	certbot renew --webroot -w /data/letsencrypt --deploy-hook 'touch /data/letsencrypt/RELOAD_NGINX'
 done
