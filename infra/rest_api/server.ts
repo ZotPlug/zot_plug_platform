e4461a2cb484be93f5695b9ed1296356ed5bf9c3
Prabhav Goyal <prabhav.goyal@gmail.com>
2025-08-18 15:08:48 -0700

    Added query/db connection in rest_api

--- infra/rest_api/server.ts
+++ infra/rest_api/server.ts
@@ -1,5 +1,5 @@
 import { Request, Response } from 'express'
-import app from './server_conf'
+import app, { pool }from './server_conf'
 import { test } from '../pg_db/postgres_actions'
 
 app.get('/', (req: Request, res: Response) => {
@@ -13,4 +13,16 @@ app.post('/api/data', (req: Request, res: Response) => {
 
 app.get('/api/data/query', async (req: Request, res: Response) => {
 	res.json({ message: "Query Recieved", yourData: await test() })
+})
+
+app.get('/api/test-db', async (req: Request, res: Response) => {
+	try {
+		const result = await pool.query('SELECT NOW()');							// Simple test query
+		res.json({message: 'Database connected!', time: result.rows[0].now });
+	
+	} catch (err) {
+		console.error(err);
+		res.status(500).json({ error: 'Database connection failed' });
+		
+	}
 })
--- infra/rest_api/server_conf.ts
+++ infra/rest_api/server_conf.ts
@@ -1,9 +1,11 @@
 import express, { Request, Response } from 'express'
 import rateLimit from 'express-rate-limit'
+import pool from '../pg_db/db_config'
 
 const PORT = 4000
 const app = express()
 const INTERNAL_IPS = ['127.0.0.1', '::1', '::ffff:127.0.0.1']
+
 const limiter = rateLimit({
 	windowMs: 15 * 60 * 1000,
 	max: 100,
@@ -11,12 +13,15 @@ const limiter = rateLimit({
 	legacyHeaders: false,
 	skip: (req) => INTERNAL_IPS.includes(req.ip ?? ""),
 })
+
 // Trust the proxy, and were one proxy hop from nginx
 app.set('trust proxy', 1)
 app.use(limiter)
 app.use(express.json())
+
 app.listen(PORT, '0.0.0.0', () => {
 	console.log(`Server running on port ${PORT}`)
 })
 
 export default app
+export { pool }
