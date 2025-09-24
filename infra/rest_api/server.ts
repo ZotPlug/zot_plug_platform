c05837449de47fb9668efe94996bd27a2ebe04d5
kchun510 <kylechun1@gmail.com>
2025-09-24 15:17:00 -0700

    Del of extra JWT MiddleWare Init + Explicit test endpoint

--- infra/rest_api/server.ts
+++ infra/rest_api/server.ts
@@ -3,11 +3,6 @@ import { Request, Response } from 'express'
 import app from './server_conf'
 import usersRouter from './routes/users'
 import devicesRouter from './routes/devices'
-import { authIfNotLocal } from './jwt_conf'
-
-// apply authIfNotLocal 
-// allows local requests and opens signup/login
-app.use(authIfNotLocal)
 
 // root route
 app.get('/', (req: Request, res: Response) => {
@@ -15,9 +10,8 @@ app.get('/', (req: Request, res: Response) => {
 })
 
 // test misc endpoint
-app.post('/api/data', (req: Request, res: Response) => {
-	console.log('Recieved: ', req.body)
-	res.json({ message: 'Data received', yourData: req.body })
+app.post('/api/test', (req: Request, res: Response) => {
+	res.json({ message: 'Data received, from test endpoint', yourData: req.body })
 })
 
 // mount resource routers
